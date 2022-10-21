f39b2d580c848228cc2daf34c2d604e508e03203
CEgelhoff <christoph.egelhoff@gmail.com>
2022-10-21 12:36:28 -0700

    hover over boxes and ask for new size

--- eas.js
+++ eas.js
@@ -1,10 +1,18 @@
 const container = document.querySelector(".pContainer")
+let rows = 16;
 
 
-for(let i = 0; i < 16; i++)
-{
+for(let i = 0; i < rows; i++)
+{  
     const square = document.createElement('div')
-    container.appendChild(square);
+    container.append(square);
     square.classList.add('grid')
     square.classList.add('grid-item-' + i)
+    square.addEventListener("mouseover", e => {
+    square.style.backgroundColor = 'black';})
+}
+
+function setLayout(){
+    rows = Number(prompt("How many rows and columns would you like?", 4))
+    rows = rows * rows
 }
